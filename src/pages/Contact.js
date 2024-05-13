95929fe5d7ff5c71bac17d4fa012a92084b093ca
tulsi <kumari20tulsi@gmail.com>
2024-05-13 14:55:47 +0530

    Initial commit

---
+++ src/pages/Contact.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+
+// function Contact() {
+//   return (
+//     <div>
+//       <h1>Contact Page</h1>
+//       <p>Contact us here.</p>
+//     </div>
+//   );
+// }
+
+// export default Contact;
+
+import Header from '../components/Header'
+export default function Contact(){
+return (
+<>
+<Header/>
+<h2>Contact Page</h2>
+</>
+)
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+
+// function Home() {
+//   return (
+//     <div>
+//       <h1>Home Page</h1>
+//       <p>Welcome to the home page!</p>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+import Header from '../components/Header'
+export default function Home(){
+return (
+<>
+<Header/>
+<h2>Home Page</h2>
+</>
+)
+}
